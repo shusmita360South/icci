230d57de672b75b3511dd0772ad6553dd6807cd6
Shusmita Anwar Sharna <smita618@yahoo.com>
2020-08-14 12:09:13 +1000

    review fixes 2

--- templates/follower/error.php
+++ templates/follower/error.php
@@ -64,23 +64,26 @@ $privacyLinky ="/privacy-policy";
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="theme-color" content="#000000">
 <jdoc:include type="head" />
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $tpath; ?>/images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $tpath; ?>/images/favicon/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $tpath; ?>/images/favicon/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $tpath; ?>/images/favicon/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $tpath; ?>/images/favicon/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $tpath; ?>/images/favicon/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $tpath; ?>/images/favicon/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $tpath; ?>/images/favicon/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tpath; ?>/images/favicon/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $tpath; ?>/images/favicon/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tpath; ?>/images/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?php echo $tpath; ?>/images/favicon/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tpath; ?>/images/favicon/favicon-16x16.png">
-<link rel="manifest" href="<?php echo $tpath; ?>/images/favicon/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?php echo $tpath; ?>/images/favicon/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-57x57.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-114x114.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-72x72.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-144x144.png" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-60x60.png" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-120x120.png" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-76x76.png" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo $tpath; ?>/assets/images/favicon/apple-touch-icon-152x152.png" />
+<link rel="icon" type="image/png" href="<?php echo $tpath; ?>/assets/images/favicon/favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="<?php echo $tpath; ?>/assets/images/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="<?php echo $tpath; ?>/assets/images/favicon/favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php echo $tpath; ?>/assets/images/favicon/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="<?php echo $tpath; ?>/assets/images/favicon/favicon-128.png" sizes="128x128" />
+<meta name="application-name" content="&nbsp;"/>
+<meta name="msapplication-TileColor" content="#FFFFFF" />
+<meta name="msapplication-TileImage" content="mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
 
 
 <link rel="stylesheet" href="<?php echo $tpath; ?>/css/font-awesome.min.css" type="text/css" />
